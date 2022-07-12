3e7daa69f3916651c77286a7b47902758cce701b
sofianebraneci <braneci1998@gmail.com>
2022-07-12 03:10:48 +0200

    Triggering the actions

--- src/main/java/common/IDrinkMaker.java
+++ src/main/java/common/IDrinkMaker.java
@@ -1,7 +1,7 @@
 package common;
 
 public interface IDrinkMaker {
-
+    // a default implementation
     static void makeDrink(String command){
         System.out.println("Received command: " + command);
     }
