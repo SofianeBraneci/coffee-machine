941ad69b88d855affb88a954a1945e69ccb1ab08
sofianebraneci <braneci1998@gmail.com>
2022-07-21 01:19:03 +0200

    Deleted current implementation, to rethink it

--- src/main/java/common/IDrinkMaker.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-public interface IDrinkMaker {
-    // a default implementation
-    static void makeDrink(String command){
-        System.out.println("Received command: " + command);
-    }
-}
